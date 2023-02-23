8beaff6d02ad593ca7356a199dde2f717973bb3e
ARodder <44817628+ARodder@users.noreply.github.com>
2023-02-23 19:12:54 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,48 @@
+// server.js
+const jsonServer = require('json-server')
+const server = jsonServer.create()
+const router = jsonServer.router('db.json')
+const middlewares = jsonServer.defaults()
+const {PORT = 3001} = process.env
+if (process.env.NODE_ENV !== 'PRODUCTION') {
+    require('dotenv').config()
+}
+
+
+const HTTP_METHOD_GET = 'get'
+
+server.use(middlewares)
+
+server.use((request, response, next) => {
+    if (request.method.toLowerCase() !== HTTP_METHOD_GET) {
+        console.log("Origin: "+ request.headers['origin'], "Looking for: "+process.env.FRONTEND_ORIGIN);
+        if(request.headers['origin'] !== process.env.FRONTEND_ORIGIN){
+            return response.status('401').json({error: 'You are not allowed to access this resource'})
+        }
+        
+
+        const token = request.headers['x-api-key'] || ''
+
+
+
+        if (!token) {
+            return response.status('401').json({error: 'You are not allowed to access this resource'})
+        }
+
+        const key = process.env.API_KEY
+
+        if (token === key) {
+            return next()
+        } else {
+            return response.status('401').json({error: 'Invalid API Key provided - are not allowed to access this resource'})
+        }
+    }
+
+    next()
+})
+
+server.use(router)
+
+server.listen(PORT, () => {
+    console.log('JSON Server is running in port: ' + PORT)
+})
